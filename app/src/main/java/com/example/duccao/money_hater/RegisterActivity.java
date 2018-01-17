c6df569474aaf9aa92a82d1f5c714c078b2109a4
Duc Cao <caominhduc610@gmail.com>
2018-01-17 23:58:14 +0700

    "Slider"

--- app/src/main/java/com/example/duccao/money_hater/RegisterActivity.java
+++ app/src/main/java/com/example/duccao/money_hater/RegisterActivity.java
@@ -1,13 +1,28 @@
 package com.example.duccao.money_hater;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    TextView txtSignin;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        txtSignin = findViewById(R.id.txtsignin);
+
+        txtSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent( RegisterActivity.this,SignInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
