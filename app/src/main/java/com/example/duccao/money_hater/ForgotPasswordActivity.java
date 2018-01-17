78ed7cd03444e2c741a41471e24226194f9667e9
Vu Duc <vuduc19081997@gmail.com>
2018-01-17 23:45:07 +0700

    "Giao dien them giao dich"

--- app/src/main/java/com/example/duccao/money_hater/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/duccao/money_hater/ForgotPasswordActivity.java
@@ -1,13 +1,25 @@
 package com.example.duccao.money_hater;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
+
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+//        btnSubmit = findViewById(R.id.btnSubmit);
+//        btnSubmit.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(ForgotPasswordActivity.this, ThemGiaoDichActivity.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
