839588f644c7dec55077c99c37a429dc2fd81a4b
Vu Duc <vuduc19081997@gmail.com>
2018-01-17 23:16:04 +0700

    Merge branch 'master' of D:\Documents\android\DATA with conflicts.

--- app/src/main/java/com/example/duccao/money_hater/LogInActivity.java
+++ app/src/main/java/com/example/duccao/money_hater/LogInActivity.java
@@ -1,13 +1,27 @@
 package com.example.duccao.money_hater;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogInActivity extends AppCompatActivity {
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_form);
+
+        btnLogin = findViewById(R.id.btnlogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogInActivity.this, ThemGiaoDichActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
