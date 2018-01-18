df5bc86a1b4664ea7e2d4082a1151fb887b79d0d
TA <t.tuananh112@gmail.com>
2018-01-18 12:47:53 +0700

    Main Screen

--- app/src/main/java/com/example/duccao/money_hater/LogInActivity.java
+++ app/src/main/java/com/example/duccao/money_hater/LogInActivity.java
@@ -3,6 +3,7 @@ package com.example.duccao.money_hater;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,8 +20,10 @@ public class LogInActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LogInActivity.this, ThemGiaoDichActivity.class);
+                Intent intent = new Intent(LogInActivity.this, MainScreen.class);
+                Log.d("test", "onClick: ");
                 startActivity(intent);
+                Log.d("test", "onClick: ");
             }
         });
     }
